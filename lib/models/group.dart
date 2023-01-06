8f3469c481b3b1b11894c07028011c1103e7a7ad
eliezerantonio <eliezer.antonio@kentratech.com>
2023-01-06 09:30:40 +0000

    Define Entity

--- lib/models/group.dart
+++ lib/models/group.dart
@@ -1,7 +1,24 @@
+import 'package:objectbox/objectbox.dart';
+
+import 'task.dart';
+
 class Group {
   int id = 0;
   String name;
   int color;
 
+  @Backlink()
+  final tasks = ToMany<Task>();
+
   Group({required this.name, required this.color});
+
+  String taskDescription() {
+    final taskCompleted = tasks.where((task) => task.completed).length;
+
+    if (tasks.isEmpty) {
+      return '';
+    }
+
+    return '$taskCompleted of ${tasks.length}';
+  }
 }
--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,7 +1,14 @@
+import 'package:objectbox/objectbox.dart';
+
+import 'group.dart';
+
+@Entity()
 class Task {
   int id = 0;
   String description;
   bool completed = false;
+
+  final group = ToOne<Group>();
 
   Task({required this.description});
 }
